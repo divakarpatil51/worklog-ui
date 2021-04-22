b3475581eded6c39beb32e289a7a6d4d101309a5
divakar51 <divakarpatil35@gmail.com>
2021-04-22 23:38:27 +0530

    Improved Reset password UI

--- src/components/ResetPassword.js
+++ src/components/ResetPassword.js
@@ -1,45 +1,52 @@
-import { Button, Grid, TextField } from '@material-ui/core';
+import { Button, Grid, TextField, Paper, Typography } from '@material-ui/core';
 import React from 'react';
 
 export default class ResetPassword extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="reset-password-background">
                 <Grid container spacing={0} justify="center" direction="column" alignItems="center">
-                    <Grid item>
-                        <div>
-                            <h1>Password Reset</h1>
-                            <h5>Enter Email id for resetting password.</h5>
-                        </div>
-                    </Grid>
-                    <Grid item>
-                        <form >
-                            <Grid container direction="column" spacing={2}>
-                                <Grid item>
-                                    <TextField
-                                        type="email"
-                                        placeholder="Email Id"
-                                        fullWidth
-                                        name="Email"
-                                        variant="outlined"
-                                        required
-                                        autoFocus
-                                    />
-                                </Grid>
-                                <Grid item style={{ marginLeft: 10 + "%" }}>
-                                    <Button
-                                        variant="contained"
-                                        color="primary"
-                                        type="submit">
-                                        Reset Password
+                    <Paper
+                        variant="elevation"
+                        elevation={2}
+                        className="reset-password-form">
+                        <Grid item>
+                            <Typography component="h5" variant="h5" style={{ marginLeft: 12 + "%" }}>
+                                Forgot Password
+                            </Typography>
+                            <Typography component="h1" variant="subtitle2">
+                                Enter Email id for resetting password.
+                            </Typography>
+                        </Grid>
+                        <Grid item style={{ marginTop: 7 + "%" }}>
+                            <form >
+                                <Grid container direction="column" spacing={2} style={{ marginTop: 10 + "%" }}>
+                                    <Grid item>
+                                        <TextField
+                                            type="email"
+                                            placeholder="Email Id"
+                                            fullWidth
+                                            name="Email"
+                                            variant="outlined"
+                                            required
+                                            autoFocus
+                                        />
+                                    </Grid>
+                                    <Grid item>
+                                        <Button
+                                            variant="contained"
+                                            color="primary"
+                                            fullWidth
+                                            type="submit">
+                                            Reset Password
                                                 </Button>
+                                    </Grid>
                                 </Grid>
-                            </Grid>
-                        </form>
-                    </Grid>
+                            </form>
+                        </Grid>
+                    </Paper>
                 </Grid>
-
             </div>
         )
     }
